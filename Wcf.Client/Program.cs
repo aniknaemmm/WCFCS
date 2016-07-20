d5fa28b354d063f816cbcd3ce92be4a7a5de4b60
aniknaemmm <aniknaemmm@gmail.com>
2016-07-20 15:36:13 +0300

    initial commit

---
+++ Wcf.Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary1;
+
+namespace Wcf.Client
+{
+    namespace Client
+    {
+        [ServiceContract]
+        public interface IMyObject
+        {
+            [OperationContract]
+            string GetCommandString(int i);
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Uri tcpUri = new Uri("http://212.98.178.83:8733/Design_Time_Addresses/WcfServiceLibrary1/Service1/");
+                EndpointAddress address = new EndpointAddress(tcpUri);
+                BasicHttpBinding binding = new BasicHttpBinding();
+                ChannelFactory<IService1> factory = new ChannelFactory<IService1>(binding, address);
+                IService1 service = factory.CreateChannel();
+                Console.WriteLine("Вызываю метод сервиса...?");
+                Console.WriteLine(service.GetData(1));
+                Console.WriteLine(service.GetData(2));
+                Console.WriteLine(service.GetData(20));
+                Console.WriteLine(service.GetData(1562492));
+                Console.WriteLine(service.GetData(0));
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Wcf.Engine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfServiceLibrary1;
+
+namespace Wcf.Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Service1), new Uri("http://212.98.178.83:8733/Design_Time_Addresses/WcfServiceLibrary1/Service1/"));
+            host.AddServiceEndpoint(typeof(Service1), new BasicHttpBinding(), "");
+            host.Open();
+            Console.WriteLine("Сервер запущен");
+            Console.ReadLine();
+
+            host.Close();
+        }
+    }
+}
