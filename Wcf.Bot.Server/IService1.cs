5cd45a96d3bc30aef3a352824a136fd4ddfd4aa7
o.grebenko <o.grebenko@itransition.com>
2016-07-22 15:22:05 +0300

    adding server for telegram bot

---
+++ Wcf.Bot.Server/IService1.cs
@@ -0,0 +1,11 @@
+﻿using WcfBot;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+[ServiceContract]
+public interface IService1
+{
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = @"/Update")]
+    void GetUpdate(Update update);
+}
---
+++ Wcf.Bot.Server/Service1.cs
@@ -0,0 +1,14 @@
+﻿namespace WcfBot
+{
+    public class Service1 : IService1
+    {
+        public void GetUpdate(Update update)
+        {
+            var Bot = new Telegram.Bot.Api("<token>");
+            if (update.message.text == "Привет")
+            {
+                Bot.SendTextMessage(update.message.chat.id, "Привет," + update.message.from.first_name);
+            }
+        }
+    }
+}
